20874034efd4bf0996633078b4eb836dfed716dd
trusty <168105018+truusty@users.noreply.github.com>
2024-05-01 23:39:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+import requests
+
+token = input("Your token here: ")
+
+user_token = token
+
+headers = {
+    "Authorization": user_token
+}
+
+response = requests.get("https://discord.com/api/v9/users/@me/relationships", headers=headers)
+
+for friend in response.json():
+    # Friend name
+    friend_name = friend['user']['username']
+    
+    # Delete Friend
+    response = requests.delete(f"https://discord.com/api/v9/users/@me/relationships/{friend['id']}", headers=headers)
+
+    # Shows Friends name
+    print(f"Friend : {friend_name}")
+
+# Shows how many friends you have left
+
+response = requests.get("https://discord.com/api/v9/users/@me/relationships", headers=headers)
+print(f"Friends Left : {len(response.json())}")
+
+print ("https://github.com/truusty")
